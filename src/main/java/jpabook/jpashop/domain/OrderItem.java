1e57bbcc611ec1aba871aae8ead5d6460525d538
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2022-03-08 20:17:29 +0900

    주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,5 +26,27 @@ public class OrderItem {
     private int count;
 
     private int orderPrice; //주문 당시 가격
+
+    //왜 여기에 order을 넣어주진않는가?
+    //무조건 order이 선행되어야(필요하진)않기때문
+    //이 객체를 만들때 미리 만들어져야하는것들만 넣어주면됨
+    public static OrderItem createOrderItem(Item item, int count, int orderPrice){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setCount(count);
+        orderItem.setOrderPrice(orderPrice);
+
+        //그리고 orderItem이 생성됐다는건.. item의 재고를 줄여줘야함으르 말함
+        item.minStock(count);
+
+        return orderItem;
+    }
+    public void cancel(){
+        //내가 주문한 수량만큼을 추가시켜주는거야
+        getItem().addStock(count);
+    }
+    public int totalPrice(){
+        return getOrderPrice() * getCount();
+    }
 }
 //ok
--- src/main/java/jpabook/jpashop/domain/Orders.java
+++ src/main/java/jpabook/jpashop/domain/Orders.java
@@ -15,7 +15,7 @@ public class Orders {
     @Id
     @GeneratedValue
     @Column(name = "orders_id")
-    private long id;
+    private Long id;
 
     //member와 orders는 일대다 관계다
     //일대다 관계 어떻게 설정하는지 잘 모르겠
@@ -36,7 +36,61 @@ public class Orders {
 
     private LocalDateTime orderDate;
 
+    //==연관관계 메서드==// - 이게 뭐지
+    //delivery나 member같은 경우 연관관계로 엮여있기때문에
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrders(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrders(this);
+    }
 
+    //주문하는 메서드 == 정적 팩토리 메서드로 static으로 선언한다
+    public static void addOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Orders order = new Orders();
+        //어떤 회원이 주문하는지 배달지는 어디인지 주문하는 아이템이 무엇인지는 매번 달라지니까 이것들을 매개변수로 삼은거야
+        order.setMember(member);
+        order.setDelivery(delivery);
+        order.setOrderDate(LocalDateTime.now());
+        for (OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+    }
+    //주문 취소하는 메서드
+    public Long cancelOrder(){
+        //근데 배송이 이미 출발했으면 취소를 못해
+        //이런 예외 터뜨리는것들도 다 비지니스 로직들이야
+        if(delivery.getStatus().equals(DeliveryStatus.COMP)){
+            //배송중인 상태인 경우 예외를 던진다
+            throw new IllegalStateException("이미 배송중인 상품은 취소할 수 없습니다");
+        }
+        setStatus(OrderStatus.Cancel);
+
+        //재고도 다시 올려줘야함
+        for(OrderItem orderItem : orderItems){
+            //내원래 생각은 아래와 같은데.. 취소 매서드를 orderItem에 따로 만든다
+            //시켰던것은 마이너스시켰을테니 그걸 다시 + 해준다
+            //orderItem.getItem().setStockQuantity(orderItem.getItem().getStockQuantity() + orderItem.getCount());
+            orderItem.cancel();
+        }
+        //어떤 주문이 취소됐는지를 알려주는게 좋을 것 같아서
+        return this.id;
+    }
+    public int getTotalPrice(){
+        int total = 0;
+        for (OrderItem orderItem : orderItems){
+            //근데 orderitem에 수량과 가격이 다있으니까 그냥 이걸 그 객체의 함수로 만들어버린다 아래와같이 코드 작성하지 말고
+            //total += orderItem.getItem().getPrice() * orderItem.getCount();
+            total += orderItem.totalPrice();
+        }
+        return total;
+    }
 
 }
 //ok
