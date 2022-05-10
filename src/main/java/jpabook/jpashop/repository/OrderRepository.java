ebdb3270404a51fb6dda8d94a38fa2c736cc0175
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2022-05-10 18:47:05 +0900

    * feat : 검색기능 개발 (+ criteria, string으로 findall하는 메서드)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,11 +1,18 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.OrderSearch;
 import jpabook.jpashop.domain.Orders;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.OneToOne;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @AllArgsConstructor
@@ -19,4 +26,79 @@ public class OrderRepository {
     public Orders findById(Long id){
         return em.find(Orders.class, id);
     }
+
+
+    //jpql로 처리
+    public List<Orders> findAllByString(OrderSearch orderSearch) {
+        //language=JPAQL
+
+        /*
+        return em.createQuery("select o from Orders o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name", Orders.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList();*/
+
+
+        String jpql = "select o From Orders o join o.member m";
+        boolean isFirstCondition = true;
+        //주문 상태 검색
+        //처음에는 그냥 where o.status = :status검색을 하는데 그이후부터는..? (그 이후가 있나? isFirstCondition왜 넣은거지?)
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Orders> query = em.createQuery(jpql, Orders.class)
+                .setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+
+    }
+    //criteria로처리 -> 무슨 jpql이 만들어지는지 한눈에 들어오지 않는다는 단점이 있음
+    public List<Orders> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Orders> cq = cb.createQuery(Orders.class);
+        Root<Orders> o = cq.from(Orders.class);
+        Join<Orders, Member> m = o.join("member", JoinType.INNER); //회원과 조인
+        List<Predicate> criteria = new ArrayList<>();
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"),
+                    orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name =
+                    cb.like(m.<String>get("name"), "%" +
+                            orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Orders> query = em.createQuery(cq).setMaxResults(1000); //최대1000건
+        return query.getResultList();
+    }
+    //쿼리dsl을 사용하자!
 }
